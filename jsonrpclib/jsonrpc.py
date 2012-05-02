d74a3cf630b354a76543714b613dd3e861a192cb
Tony Barbieri <Tony Barbieri>
2012-05-02 15:57:32 -0400

    added omnijson

--- jsonrpclib/jsonrpc.py
+++ jsonrpclib/jsonrpc.py
@@ -61,22 +61,7 @@ import jsonrpclib
 from jsonrpclib import config
 from jsonrpclib import history
 
-# JSON library importing
-cjson = None
-json = None
-try:
-    import cjson
-except ImportError:
-    try:
-        import json
-    except ImportError:
-        try:
-            import simplejson as json
-        except ImportError:
-            raise ImportError(
-                'You must have the cjson, json, or simplejson ' +
-                'module(s) available.'
-            )
+import omnijson as json
 
 IDCHARS = string.ascii_lowercase+string.digits
 
